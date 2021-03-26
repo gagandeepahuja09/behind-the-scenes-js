618bb3e70ac97d565df6477b4144c2cb370ce0d4
gagandeep.ahuja <gagandeep.ahuja@whiteharjr.com>
2021-03-26 17:09:45 +0530

    primitives vs reference types example

--- script.js
+++ script.js
@@ -1,23 +1,79 @@
-const jonas = {
-  year: 1980,
-  calcAge: function() {
-    return 2021 - this.year
-  }
+// const jonas = {
+//   year: 1980,
+//   calcAge: function() {
+//     return 2021 - this.year
+//   }
+// }
+
+// console.log(jonas.calcAge())
+
+// const gary = {
+//   year: 1998
+// }
+
+// gary.calcAge = jonas.calcAge
+
+// console.log(gary.calcAge())
+
+// // Note: only var variable gets added as property to the window object 
+// var year = 2030
+
+// const calcAgeGlobal = jonas.calcAge
+
+// console.log(calcAgeGlobal())
+
+// arguments is not accessible in arrow functions
+const addExpr = function(a, b) {
+  console.log(arguments)
+  return a + b
 }
 
-console.log(jonas.calcAge())
+addExpr(1, 2, 3, 4, 5, 6)
 
-const gary = {
-  year: 1998
+// It's a misconception that all values declared with const are immutable.
+// It's only true for primitive types and not for reference types.
+
+// Reference types ==> Objects can be very large, hence they are stored in heap memory
+// While primitive types are stored directly in the variable environment.
+
+// Primitive values example
+let age = 30
+oldAge = age
+age = 31
+console.log(age, oldAge)
+
+const me = {
+  name: 'Gagandeep',
+  age: 22
 }
 
-gary.calcAge = jonas.calcAge
+const friend = me
+friend.age = 24
 
-console.log(gary.calcAge())
+console.log({ friend, me })
 
-// Note: only var variable gets added as property to the window object 
-var year = 2030
+/*
 
-const calcAgeGlobal = jonas.calcAge
+Identifier          Address             Value
+age, oldAge         0001                30
+------
+Later
+------
+Identifier          Address             Value
+oldAge              0001                30
+age                 0002                20
+me, friend          0003                D30F(Reference to memory address in heap)
 
-console.log(calcAgeGlobal())
+
+Heap
+Address         Value
+D30F            { name: 'Gagandeep', age = 24 }
+
+*/
+
+
+/* 
+  Prototypal Inheritance
+  Event Loop
+  How DOM really works
+*/
